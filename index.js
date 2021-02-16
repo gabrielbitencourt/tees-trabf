d3a3afd15f0600bbf6557dde53f36cd4d083d464
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2021-02-16 03:27:10 -0300

    versao 2 com novo algoritmo de identificacao de sequencias, preenchimento de espacos vazios, fluxo de jogo e de codigos melhorados, mais funcoes e funcoes mais claras na classe do jogo

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ class Game {
         this.offsetX = (this.canvas.width - (CONSTS.COLS * CONSTS.CELL_SIZE + 1) - 1) / 2;
         this.offsetY = (this.canvas.height - (CONSTS.ROWS * CONSTS.CELL_SIZE + 1) - 1) / 2;
 
+        this.bounds = {
+            minX: this.offsetX,
+            minY: this.offsetY,
+            maxX: this.offsetX + (CONSTS.COLS * (CONSTS.CELL_SIZE + 1)),
+            maxY: this.offsetY + (CONSTS.ROWS * (CONSTS.CELL_SIZE + 1))
+        };
+
         this.frame = null;
         this.running = false;
 
@@ -67,7 +74,7 @@ class Game {
         }
 
         this.drawGrid();
-        this.checkSequences(0, 0);
+        this.findAndRemoveAllSequences();
     }
 
     start() {
@@ -75,6 +82,7 @@ class Game {
         window.onresize = this.resizeHandler;
         this.canvas.onclick = this.clickHandler;
         this.loop();
+        this.prevState = this;
     }
 
     clear() {
@@ -88,6 +96,12 @@ class Game {
 
         this.offsetX = (this.canvas.width - (CONSTS.COLS * CONSTS.CELL_SIZE + 1) - 1) / 2;
         this.offsetY = (this.canvas.height - (CONSTS.ROWS * CONSTS.CELL_SIZE + 1) - 1) / 2;
+        this.bounds = {
+            minX: this.offsetX,
+            minY: this.offsetY,
+            maxX: this.offsetX + (CONSTS.COLS * (CONSTS.CELL_SIZE + 1)),
+            maxY: this.offsetY + (CONSTS.ROWS * (CONSTS.CELL_SIZE + 1))
+        };
         this.loop();
     }
 
@@ -96,18 +110,12 @@ class Game {
      * @param {MouseEvent} ev 
      */
     async clickHandler(ev) {
-        const bounds = {
-            minX: this.offsetX,
-            minY: this.offsetY,
-            maxX: this.offsetX + (CONSTS.COLS * (CONSTS.CELL_SIZE + 1)),
-            maxY: this.offsetY + (CONSTS.ROWS * (CONSTS.CELL_SIZE + 1))
-        };
-
-        if (ev.clientX > bounds.minX && ev.clientX < bounds.maxX && ev.clientY > bounds.minY && ev.clientY < bounds.maxY) {
-            const x = Math.floor((ev.clientX - bounds.minX) / CONSTS.CELL_SIZE);
-            const y = Math.floor((ev.clientY - bounds.minY) / CONSTS.CELL_SIZE);
+        if (ev.clientX > this.bounds.minX && ev.clientX < this.bounds.maxX && ev.clientY > this.bounds.minY && ev.clientY < this.bounds.maxY) {
+            const x = Math.floor((ev.clientX - this.bounds.minX) / CONSTS.CELL_SIZE);
+            const y = Math.floor((ev.clientY - this.bounds.minY) / CONSTS.CELL_SIZE);
 
             if (!this.selected) {
+                this.prevState = JSON.parse(JSON.stringify(this.grid));
                 this.grid[y][x].selected = true;
                 this.selected = { x, y };
             }
@@ -115,24 +123,54 @@ class Game {
                 if (this.grid[y][x].selected === false) {
                     if ((x === this.selected.x && Math.abs(y - this.selected.y) === 1) || (y === this.selected.y && Math.abs(x - this.selected.x) === 1)) {
                         // check if theres sequences
-
                         const temp = this.grid[y][x];
                         this.grid[y][x] = this.grid[this.selected.y][this.selected.x];
                         this.grid[this.selected.y][this.selected.x] = temp;
                         
-                        const points = await this.checkSequences();
+                        this.loop();
+                        let sequences = this.checkCell({ row: y, col: x });
+                        sequences.push(...this.checkCell({ row: this.selected.y, col: this.selected.x }, sequences));
+                        await this.clenSequences(sequences);
+
+                        let points = sequences.reduce((prev, sequence) => prev = prev + sequence.cells.length, 0);
                         if (points === 0) {
                             this.grid[this.selected.y][this.selected.x] = this.grid[y][x];
                             this.grid[y][x] = temp;
                         }
                         else {
-                            this.points = this.points + points;
-                            this.onPointsScored(this.points);
+                            while (sequences.length) {
+                                this.loop();
+                                const cellsModified = [];
+
+                                this.points = this.points + points;
+                                this.onPointsScored(this.points);
+
+                                for (const sequence of sequences) {
+                                    for (const sequenceCell of sequence.cells) {
+                                        const modifiedInRow = await this.pullRow(sequenceCell.row, sequenceCell.col);
+                                        if (sequence.direction === 'horizontal' || sequenceCell === sequence.cells[sequence.cells.length - 1]) {
+                                            cellsModified.push(...modifiedInRow);
+                                        }
+                                    }
+                                }
+                                // check every cell of the rows modified starting from cell col to 0
+                                // after the check, get all new sequences and pull those rows and repeat
+                                sequences = [];
+                                for (const modifiedCell of cellsModified) {
+                                    this.loop();
+                                    const newSequences = this.checkCell(modifiedCell, sequences);
+                                    sequences.push(...newSequences);
+                                    if (newSequences.length) await sleep(200);
+                                }
+                                await this.clenSequences(sequences);
+                                points = sequences.reduce((prev, sequence) => prev = prev + sequence.cells.length, 0);
+                            }
                         }
 
                         this.grid[y][x].selected = false;
                         this.grid[this.selected.y][this.selected.x].selected = false;
                         this.selected = null;
+                        this.loop();
                     }
                     else {
                         console.error('jogada não permitida');
@@ -143,18 +181,12 @@ class Game {
                     this.selected = null;
                 }
             }
-            
         }
         this.loop();
     }
 
-    /**
-     * 
-     * @param {number?} time animation frame time when used with requestAnimationFrame
-     */
-    loop(time) {
+    loop() {
         this.clear();
-
         this.ctx.beginPath();
         this.drawGrid();
     }
@@ -215,107 +247,189 @@ class Game {
         if (stroke) this.ctx.stroke();
     }
 
-    /**
-     * 
-     * @param {number} acc score accumulator for recursive calls
-     * 
-     * @returns {Promise<number>} number of cells scored
-     */
-    async checkSequences(acc = 0, timeout = 500) {
-        let score = acc;
+    async findAndRemoveAllSequences() {
         for (let r = 0; r < CONSTS.ROWS; r++) {
             const row = this.grid[r];
             for (let c = 0; c < CONSTS.COLS; c++) {
                 const cell = row[c];
-                let sequenceV = 1;
-                let sequenceH = 1;
 
+                let sequenceH = 1;
                 let walkingC = c + 1;
                 while (walkingC < CONSTS.COLS && row[walkingC].color === cell.color) {
                     sequenceH = sequenceH + 1;
                     walkingC = walkingC + 1;
                 }
 
+                let sequenceV = 1;
                 let walkingR = r + 1;
                 while (walkingR < CONSTS.ROWS && this.grid[walkingR][c].color === cell.color) {
                     sequenceV = sequenceV + 1;
                     walkingR = walkingR + 1;
                 }
 
-
                 if (sequenceH >= 3) {
-                    console.log(`${cell.color} vertical sequence of ${sequenceH} from (${r + 1}, ${c + 1})`);
-                    score = score + sequenceH;
-
                     for (let index = 0; index < sequenceH; index++) {
                         this.grid[r][c + index] = null;
                     }
-                    this.loop();
-                    await sleep(timeout);
-
-
-                    let walkingS = sequenceH - 1;
-                    while (walkingS >= 0) {
-                        walkingR = r;
-                        while (walkingR > 0) {
-                            this.grid[walkingR][c + walkingS] = this.grid[walkingR - 1][c + walkingS];
-                            this.grid[walkingR - 1][c + walkingS] = null;
-                            walkingR = walkingR - 1;
-                        }
-                        this.grid[walkingR][c + walkingS] = new Cell();
-                        walkingS = walkingS - 1;
+                    for (let index = 0; index < sequenceH; index++) {
+                        await this.pullRow(r, c + index, false);
                     }
                 }
                 if (sequenceV >= 3) {
-                    console.log(`${cell.color} horizontal sequence of ${sequenceV} from (${r + 1}, ${c + 1})`);
-                    score = score + sequenceV;
-
                     for (let index = 0; index < sequenceV; index++) {
                         this.grid[r + index][c] = null;
                     }
-                    this.loop();
-                    await sleep(timeout);
-                    
-                    let walkingS = 0;
-                    while (walkingS < sequenceV) {
-                        walkingR = r + walkingS;
-                        while (walkingR > 0) {
-                            this.grid[walkingR][c] = this.grid[walkingR - 1][c];
-                            this.grid[walkingR - 1][c] = null;
-                            walkingR = walkingR - 1;
-                        }
-                        this.grid[walkingR][c] = new Cell();
-                        walkingS = walkingS + 1;
+                    for (let index = 0; index < sequenceV; index++) {
+                        await this.pullRow(r + index, c, false);
                     }
                 }
 
                 if (sequenceH >= 3 || sequenceV >= 3) {
-                    this.loop();
-                    await sleep(timeout);
-                    return await this.checkSequences(score);
+                    return await this.findAndRemoveAllSequences();
                 }
             }
         }
         this.loop();
-        return score;
+    }
+
+    /**
+     * 
+     * @param {{ col: number, row: number }} cell 
+     * 
+     */
+    checkCell(cell, sequencesMade = []) {
+        if (!this.grid[cell.row][cell.col]) return [];
+        const color = this.grid[cell.row][cell.col].color;
+        const sequences = [];
+
+        // sequencia vertical
+        let pointer = { row: cell.row, col: cell.col };
+        while (pointer.row > 0 && this.grid[pointer.row - 1][pointer.col] && this.grid[pointer.row][pointer.col].color === this.grid[pointer.row - 1][pointer.col].color) {
+            pointer.row = pointer.row - 1;
+        }
+        
+        const sameVerticalSequence = sequencesMade
+                .filter(sequence => sequence.direction === 'vertical')
+                .find(sequence => sequence.cells[0].row === pointer.row && sequence.cells[0].col === pointer.col);
+        if (!sameVerticalSequence) {
+            let sequenceCells = [{
+                row: pointer.row,
+                col: pointer.col
+            }];
+            while (pointer.row + 1 < CONSTS.ROWS && this.grid[pointer.row + 1][pointer.col] && this.grid[pointer.row + 1][pointer.col].color === color) {
+                sequenceCells.push({
+                    row: pointer.row + 1,
+                    col: pointer.col
+                });
+                pointer.row = pointer.row + 1;
+            }
+            if (sequenceCells.length >= 3) {
+                console.log(`${color} vertical sequence of ${sequenceCells.length} from (${pointer.row + 1}, ${pointer.col + 1})`);
+                sequences.push({
+                    cells: sequenceCells,
+                    direction: 'vertical'
+                });
+            }
+        }
+
+
+        // sequencia horizontal
+        pointer = { row: cell.row, col: cell.col };
+        while (pointer.col > 0 && this.grid[pointer.row][pointer.col - 1] && this.grid[pointer.row][pointer.col].color === this.grid[pointer.row][pointer.col - 1].color) {
+            pointer.col = pointer.col - 1;
+        }
+
+        const sameHorizontalSequence = sequencesMade
+            .filter(sequence => sequence.direction === 'horizontal')
+            .find(sequence => sequence.cells[0].row === pointer.row && sequence.cells[0].col === pointer.col);
+        if (!sameHorizontalSequence) {
+            let sequenceCells = [{
+                row: pointer.row,
+                col: pointer.col
+            }];
+    
+            while (pointer.col + 1 < CONSTS.COLS && this.grid[cell.row][pointer.col + 1] && this.grid[cell.row][pointer.col + 1].color === color) {
+                sequenceCells.push({
+                    row: pointer.row,
+                    col: pointer.col + 1
+                });
+                pointer.col = pointer.col + 1;
+            }
+    
+            if (sequenceCells.length >= 3) {
+                console.log(`${color} horizontal sequence of ${sequenceCells.length} from (${pointer.row + 1}, ${pointer.col + 1})`);
+                sequences.push({
+                    cells: sequenceCells,
+                    direction: 'horizontal'
+                });
+            }
+        }
+        return sequences;
+    }
+
+    async clenSequences(sequences, timeout = true) {
+        for (const sequence of sequences) {
+            for (const cell of sequence.cells) {
+                this.grid[cell.row][cell.col] = null;
+            }
+        }
+        if (timeout) {
+            this.loop();
+            await sleep(150);
+        }
+    }
+
+    async pullRow(row, col, timeout = true) {
+        if (this.grid[row][col]) return [];
+        let newCells = 1;
+        let cellsModified = [];
+        let pointer = row;
+        outer:
+        while (pointer - newCells >= 0) {
+            while (this.grid[pointer - newCells][col] === null) {
+                newCells = newCells + 1;
+                if (pointer - newCells <= 0) break outer;
+            }
+            this.grid[pointer][col] = this.grid[pointer - newCells][col];
+            cellsModified.push({ col, row: pointer });
+            pointer = pointer - newCells;
+            this.loop();
+        }
+
+        for (let index = pointer; index < newCells; index++) {
+            this.grid[index][col] = new Cell();
+            cellsModified.push({ col, row: index });
+        }
+        
+        if (timeout) {
+            this.loop();
+            await sleep(150);
+        }
+        return cellsModified;
     }
 }
 
 const canvas = document.querySelector('canvas#game');
 const pointsLabel = document.querySelector('p#points');
-const game = new Game(canvas);
-game.start();
+
+let game = new Game(canvas);
 game.onPointsScored = (points) => {
     pointsLabel.innerHTML = `${points} pontos`;
 };
+game.start();
 
 function restart() {
     pointsLabel.innerHTML = `0 pontos`;
-    const game = new Game(canvas);
-    game.start();
+    game = new Game(canvas);
     game.onPointsScored = (points) => {
         pointsLabel.innerHTML = `${points} pontos`;
     };
+    game.start();
+}
+
+function back() {
+    game.grid = game.prevState;
+    game.loop();
 }
 
 async function sleep(ms) {
